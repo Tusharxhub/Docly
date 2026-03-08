92e9a5d784307475168ab9829ca6980608ec7520
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-08 15:50:26 +0530

    chore: update prisma dependency from 6.19.2 to 7.4.2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,9 +2,13 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString, ssl: { rejectUnauthorized: true } });
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export { prisma };
